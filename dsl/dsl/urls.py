5c052c50e48b4941ba26dbdc67c1ce377db0463b
LightlanaDr <145448278+LightlanaDr@users.noreply.github.com>
2024-02-17 12:34:15 +0300

    Update urls.py

--- dsl/dsl/urls.py
+++ dsl/dsl/urls.py
@@ -1,24 +1,14 @@
-"""
-URL configuration for dsl project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from .sitemaps import StaticSitemap
+from .views import robots_txt, pageNotFound
+
+sitemaps = {'products': StaticSitemap, }
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,8 +16,11 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('catalog/', include('catalog.urls')),
     path('gallery/', include('gallery.urls')),
-
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', robots_txt),
 ]
+
+handler404 = pageNotFound
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
