d4d0f70051fdbafb7cafb918d80e1db014eb62c3
LightlanaDr <145448278+LightlanaDr@users.noreply.github.com>
2024-02-17 12:32:30 +0300

    Update views.py

--- dsl/main/views.py
+++ dsl/main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .forms import ContactForm
 
 def index(request):
     data = {
@@ -10,3 +10,17 @@ def index(request):
 
 def delivery(request):
     return render(request, 'main/delivery.html')
+
+def contact(request):
+    return render(request, 'main/contact_dsl.html')
+
+def send_message(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            contact = form.save()
+            contact.save()
+            return render(request, 'main/created_mes.html', {'contact': contact})
+    else:
+        form = ContactForm()
+    return render(request, 'main/send_message.html', {'form': form})
