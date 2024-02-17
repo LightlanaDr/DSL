cf0f2b1a443614ce9b445c2fab50f6cb385c6870
LightlanaDr <145448278+LightlanaDr@users.noreply.github.com>
2024-02-17 12:31:21 +0300

    Update models.py

--- dsl/main/models.py
+++ dsl/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    tel = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    message = models.TextField(max_length=1000)
+    date_create = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        # Будет отображаться следующее поле в панели администрирования
+        return self.email
